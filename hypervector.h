3aaaf7091fadc5a0a49f06eee9e157b31b12e42c
Marco Porsch <marco.porsch@posteo.de>
2024-06-08 10:54:31 +0200

    rename arguments and internals

--- hypervector.h
+++ hypervector.h
@@ -17,7 +17,7 @@ struct hypervector_detail
 };
 
 /// view on hypervector storage providing element read and write accessors
-template<typename T, size_t N, bool IsConst>
+template<typename T, size_t Dims, bool IsConst>
 class hypervector_view
 {
 public:
@@ -29,12 +29,11 @@ public:
   using iterator = typename std::conditional<IsConst,
     const_iterator,
     typename container::iterator>::type;
-
-  using const_slice = typename std::conditional<(N > 1),
-    hypervector_view<T, N - 1, true>,
+  using const_slice = typename std::conditional<(Dims > 1),
+    hypervector_view<T, Dims - 1, true>,
     void>::type;
-  using slice = typename std::conditional<(N > 1),
-    hypervector_view<T, N - 1, IsConst>,
+  using slice = typename std::conditional<(Dims > 1),
+    hypervector_view<T, Dims - 1, IsConst>,
     void>::type;
 
 public:
@@ -42,83 +41,83 @@ public:
 
 
   hypervector_view(
-      const size_type* dims,
+      const size_type* sizes,
       const size_type* offsets,
       iterator first)
-    : dims_(dims)
+    : sizes_(sizes)
     , offsets_(offsets)
     , first_(first) {
   }
 
 
   // reference at(size_type pos)
-  template<typename ...Args>
-  typename std::enable_if<sizeof...(Args) == N, reference>::type
-  at(Args&&... args) {
-    return *(first_ + indexOf_(std::forward<Args>(args)...));
+  template<typename ...Indices>
+  typename std::enable_if<sizeof...(Indices) == Dims, reference>::type
+  at(Indices&&... indices) {
+    return *(first_ + indexOf_(std::forward<Indices>(indices)...));
   }
 
 
   // const_reference at(size_type pos) const
-  template<typename ...Args>
-  typename std::enable_if<sizeof...(Args) == N, const_reference>::type
-  at(Args&&... args) const {
-    return *(first_ + indexOf_(std::forward<Args>(args)...));
+  template<typename ...Indices>
+  typename std::enable_if<sizeof...(Indices) == Dims, const_reference>::type
+  at(Indices&&... indices) const {
+    return *(first_ + indexOf_(std::forward<Indices>(indices)...));
   }
 
 
   // subdimension operator[](size_type pos)
-  template<size_t N_ = N,
-           typename = typename std::enable_if<(N_ > 1)>::type>
+  template<size_t Dims_ = Dims,
+           typename = typename std::enable_if<(Dims_ > 1)>::type>
   slice operator[](size_type pos) {
     return slice(
-      dims_ + 1,
+      sizes_ + 1,
       offsets_ + 1,
       first_ + pos * offsets_[0]);
   }
 
-  template<size_t N_ = N,
-           typename = typename std::enable_if<(N_ == 1)>::type>
+  template<size_t Dims_ = Dims,
+           typename = typename std::enable_if<(Dims_ == 1)>::type>
   reference operator[](size_type pos) {
     return *(first_ + pos);
   }
 
 
   // subdimension operator[](size_type pos) const
-  template<size_t N_ = N,
-           typename = typename std::enable_if<(N_ > 1)>::type>
+  template<size_t Dims_ = Dims,
+           typename = typename std::enable_if<(Dims_ > 1)>::type>
   const_slice operator[](size_type pos) const {
     return const_slice(
-      dims_ + 1,
+      sizes_ + 1,
       offsets_ + 1,
       first_ + pos * offsets_[0]);
   }
 
-  template<size_t N_ = N,
-           typename = typename std::enable_if<(N_ == 1)>::type>
+  template<size_t Dims_ = Dims,
+           typename = typename std::enable_if<(Dims_ == 1)>::type>
   const_reference operator[](size_type pos) const {
     return *(first_ + pos);
   }
 
 
   size_type size() const {
-    return std::accumulate(dims_, dims_ + N, 1,
-      [](size_type prod, size_type dim) -> size_type {
-        return prod * dim;
+    return std::accumulate(sizes_, sizes_ + Dims, 1,
+      [](size_type prod, size_type size) -> size_type {
+        return prod * size;
       });
   }
 
 
   template<size_type Dim>
   size_type sizeOf() const {
-    static_assert(Dim <= N, "hypervector_view::sizeOf");
-    return dims_[Dim];
+    static_assert(Dim <= Dims, "hypervector_view::sizeOf");
+    return sizes_[Dim];
   }
 
 
   template<size_type Dim>
   size_type offsetOf() const {
-    static_assert(Dim <= N, "hypervector_view::offsetOf");
+    static_assert(Dim <= Dims, "hypervector_view::offsetOf");
     return offsets_[Dim];
   }
 
@@ -144,40 +143,40 @@ public:
 
 
   // implicit conversion from non-const to const
-  operator hypervector_view<T, N, true>() const
+  operator hypervector_view<T, Dims, true>() const
   {
-    return hypervector_view<T, N, true>(dims_, offsets_, first_);
+    return hypervector_view<T, Dims, true>(sizes_, offsets_, first_);
   }
 
 protected:
-  template<typename ...Args>
-  typename std::enable_if<sizeof...(Args) <= N - 1, size_type>::type
+  template<typename ...Indices>
+  typename std::enable_if<sizeof...(Indices) <= Dims - 1, size_type>::type
   indexOf_(
-      size_type dim,
-      Args&&... args) const {
-    constexpr auto idx = N - sizeof...(Args) - 1;
-    if(dim >= dims_[idx])
+      size_type index0,
+      Indices&&... indices) const {
+    constexpr auto dim = Dims - sizeof...(Indices) - 1;
+    if(index0 >= sizes_[dim])
       throw std::out_of_range("hypervector_view::at");
-    return dim * offsets_[idx] + indexOf_(std::forward<Args>(args)...);
+    return index0 * offsets_[dim] + indexOf_(std::forward<Indices>(indices)...);
   }
 
-  size_type indexOf_(size_type dim) const {
-    return dim;
+  size_type indexOf_(size_type index) const {
+    return index;
   }
 
 protected:
-  const size_type* dims_;
+  const size_type* sizes_;
   const size_type* offsets_;
   iterator first_;
 };
 
 
 /// hypervector container providing storage size modifiers
-template<typename T, size_t N>
-class hypervector : public hypervector_view<T, N, false>
+template<typename T, size_t Dims>
+class hypervector : public hypervector_view<T, Dims, false>
 {
 public:
-  using view = hypervector_view<T, N, false>;
+  using view = hypervector_view<T, Dims, false>;
   using size_type = typename hypervector_detail<T>::size_type;
   using container = typename hypervector_detail<T>::container;
 
@@ -185,33 +184,33 @@ public:
   // hypervector()
   /// create uninitialized container
   hypervector()
-    : dims_{0}
+    : sizes_{0}
     , offsets_{0} {
-    static_cast<view&>(*this) = view(dims_.data(), offsets_.data(), vec_.begin());
+    static_cast<view&>(*this) = view(sizes_.data(), offsets_.data(), vec_.begin());
   }
 
 
   // hypervector(size_type count, const T& value)
   /// create container with given dimensions;
   /// values are initialized to given value
-  template<typename ...Args>
+  template<typename ...Sizes>
   hypervector(
-      typename std::enable_if<sizeof...(Args) == N, size_type>::type dim0,
-      Args&&... args) {
-    static_cast<view&>(*this) = view(dims_.data(), offsets_.data(), vec_.begin());
-    (void)assign_(1, dim0, std::forward<Args>(args)...);
+      typename std::enable_if<sizeof...(Sizes) == Dims, size_type>::type size0,
+      Sizes&&... sizes) {
+    static_cast<view&>(*this) = view(sizes_.data(), offsets_.data(), vec_.begin());
+    (void)assign_(1, size0, std::forward<Sizes>(sizes)...);
   }
 
 
   // hypervector(size_type count)
   /// create container with given dimensions;
   /// values are default initialized
-  template<typename ...Args>
+  template<typename ...Sizes>
   hypervector(
-      typename std::enable_if<sizeof...(Args) == N - 1, size_type>::type dim0,
-      Args&&... args) {
-    static_cast<view&>(*this) = view(dims_.data(), offsets_.data(), vec_.begin());
-    (void)assign_(1, dim0, std::forward<Args>(args)..., T());
+      typename std::enable_if<sizeof...(Sizes) == Dims - 1, size_type>::type size0,
+      Sizes&&... sizes) {
+    static_cast<view&>(*this) = view(sizes_.data(), offsets_.data(), vec_.begin());
+    (void)assign_(1, size0, std::forward<Sizes>(sizes)..., T());
   }
 
 
@@ -220,35 +219,35 @@ public:
 
 
   hypervector(const hypervector& other)
-    : dims_(other.dims_)
+    : sizes_(other.sizes_)
     , offsets_(other.offsets_)
     , vec_(other.vec_) {
-    static_cast<view&>(*this) = view(dims_.data(), offsets_.data(), vec_.begin());
+    static_cast<view&>(*this) = view(sizes_.data(), offsets_.data(), vec_.begin());
   }
 
 
   hypervector(hypervector&& other)
-    : dims_(std::move(other.dims_))
+    : sizes_(std::move(other.sizes_))
     , offsets_(std::move(other.offsets_))
     , vec_(std::move(other.vec_)) {
-    static_cast<view&>(*this) = view(dims_.data(), offsets_.data(), vec_.begin());
+    static_cast<view&>(*this) = view(sizes_.data(), offsets_.data(), vec_.begin());
   }
 
 
   hypervector& operator=(const hypervector& other) {
-    dims_ = other.dims_;
+    sizes_ = other.sizes_;
     offsets_ = other.offsets_;
     vec_ = other.vec_;
-    static_cast<view&>(*this) = view(dims_.data(), offsets_.data(), vec_.begin());
+    static_cast<view&>(*this) = view(sizes_.data(), offsets_.data(), vec_.begin());
     return *this;
   }
 
 
   hypervector& operator=(hypervector&& other) {
-    dims_ = std::move(other.dims_);
+    sizes_ = std::move(other.sizes_);
     offsets_ = std::move(other.offsets_);
     vec_ = std::move(other.vec_);
-    static_cast<view&>(*this) = view(dims_.data(), offsets_.data(), vec_.begin());
+    static_cast<view&>(*this) = view(sizes_.data(), offsets_.data(), vec_.begin());
     return *this;
   }
 
@@ -256,47 +255,47 @@ public:
   // void resize(size_type count, const T& value)
   /// resize container to given dimensions;
   /// newly created elements will be initialized to given value
-  template<typename ...Args>
-  typename std::enable_if<sizeof...(Args) == N, void>::type
+  template<typename ...Sizes>
+  typename std::enable_if<sizeof...(Sizes) == Dims, void>::type
   resize(
-      size_type dim0,
-      Args&&... args) {
-    (void)resize_(1, dim0, std::forward<Args>(args)...);
+      size_type size0,
+      Sizes&&... sizes) {
+    (void)resize_(1, size0, std::forward<Sizes>(sizes)...);
   }
 
 
   // void resize(size_type count)
   /// resize container to given dimensions;
   /// newly created elements will be default initialized
-  template<typename ...Args>
-  typename std::enable_if<sizeof...(Args) == N - 1, void>::type
+  template<typename ...Sizes>
+  typename std::enable_if<sizeof...(Sizes) == Dims - 1, void>::type
   resize(
-      size_type dim0,
-      Args&&... args) {
-    (void)resize_(1, dim0, std::forward<Args>(args)..., T());
+      size_type size0,
+      Sizes&&... sizes) {
+    (void)resize_(1, size0, std::forward<Sizes>(sizes)..., T());
   }
 
 
   // void assign(size_type count, const T& value)
   /// assign given dimensions to container and
   /// set all elements to given value
-  template<typename ...Args>
-  typename std::enable_if<sizeof...(Args) == N, void>::type
+  template<typename ...Sizes>
+  typename std::enable_if<sizeof...(Sizes) == Dims, void>::type
   assign(
-      size_type dim0,
-      Args&&... args) {
-    (void)assign_(1, dim0, std::forward<Args>(args)...);
+      size_type size0,
+      Sizes&&... sizes) {
+    (void)assign_(1, size0, std::forward<Sizes>(sizes)...);
   }
 
 
   // void reserve(size_type count)
   /// reserve container to given maximum dimension sizes to pre-allocate storage
-  template<typename ...Args>
-  typename std::enable_if<sizeof...(Args) == N - 1, void>::type
+  template<typename ...Sizes>
+  typename std::enable_if<sizeof...(Sizes) == Dims - 1, void>::type
   reserve(
-      size_type dim0,
-      Args&&... args) {
-    reserve_(1, dim0, std::forward<Args>(args)...);
+      size_type size0,
+      Sizes&&... sizes) {
+    reserve_(1, size0, std::forward<Sizes>(sizes)...);
   }
 
 
@@ -307,16 +306,16 @@ public:
   }
 
 private:
-  template<typename ...Args>
-  typename std::enable_if<sizeof...(Args) <= N, size_type>::type
+  template<typename ...Sizes>
+  typename std::enable_if<sizeof...(Sizes) <= Dims, size_type>::type
   resize_(
       size_type size,
-      size_type dim,
-      Args&&... args) {
-    constexpr auto idx = N - sizeof...(Args);
-    dims_[idx] = dim;
-    offsets_[idx] = resize_(size * dim, std::forward<Args>(args)...);
-    return offsets_[idx] * dim;
+      size_type size0,
+      Sizes&&... sizes) {
+    constexpr auto dim = Dims - sizeof...(Sizes);
+    sizes_[dim] = size0;
+    offsets_[dim] = resize_(size * size0, std::forward<Sizes>(sizes)...);
+    return offsets_[dim] * size0;
   }
 
   size_type resize_(
@@ -328,16 +327,16 @@ private:
   }
 
 
-  template<typename ...Args>
-  typename std::enable_if<sizeof...(Args) <= N, size_type>::type
+  template<typename ...Sizes>
+  typename std::enable_if<sizeof...(Sizes) <= Dims, size_type>::type
   assign_(
       size_type size,
-      size_type dim,
-      Args&&... args) {
-    constexpr auto idx = N - sizeof...(Args);
-    dims_[idx] = dim;
-    offsets_[idx] = assign_(size * dim, std::forward<Args>(args)...);
-    return offsets_[idx] * dim;
+      size_type size0,
+      Sizes&&... sizes) {
+    constexpr auto dim = Dims - sizeof...(Sizes);
+    sizes_[dim] = size0;
+    offsets_[dim] = assign_(size * size0, std::forward<Sizes>(sizes)...);
+    return offsets_[dim] * size0;
   }
 
   size_type assign_(
@@ -349,13 +348,13 @@ private:
   }
 
 
-  template<typename ...Args>
-  typename std::enable_if<sizeof...(Args) < N, void>::type
+  template<typename ...Sizes>
+  typename std::enable_if<sizeof...(Sizes) < Dims, void>::type
   reserve_(
       size_type size,
-      size_type dim,
-      Args&&... args) {
-    reserve_(size * dim, std::forward<Args>(args)...);
+      size_type size0,
+      Sizes&&... sizes) {
+    reserve_(size * size0, std::forward<Sizes>(sizes)...);
   }
 
   void reserve_(size_type size) {
@@ -364,16 +363,16 @@ private:
   }
 
 private:
-  std::array<size_type, N> dims_;
-  std::array<size_type, N> offsets_;
+  std::array<size_type, Dims> sizes_;
+  std::array<size_type, Dims> offsets_;
   container vec_;
 };
 
 
-template<typename T, size_t N, bool IsConst>
+template<typename T, size_t Dims, bool IsConst>
 std::ostream& operator<<(
     std::ostream& os,
-    const hypervector_view<T, N, IsConst>& hvec) {
+    const hypervector_view<T, Dims, IsConst>& hvec) {
   auto size = hvec.template sizeOf<0>();
   const char* separator = "";
   for(decltype(size) i = 0; i < size; ++i) {
